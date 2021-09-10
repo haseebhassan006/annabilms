3feea566faf39d8cca948ecc65525bc987ba1a1c
Hassoku <mhhassan095@gmail.com>
2021-09-10 15:10:57 -0700

    astaghfar

--- resources/lang/en/meeting.php
+++ resources/lang/en/meeting.php
@@ -11,7 +11,6 @@ return [
     'meeting_setting_page_title' => 'Meeting Setting',
     'meeting_list_page_title' => 'Meetings List',
     'meeting_requests_page_title' => 'Meeting requests list',
-
     'dont_exist_time' => 'Time not exists',
     'locked_time' => 'Access denied, This time is not available!',
     'reserved_time' => 'Access denied, This time already reserved!',
@@ -20,7 +19,6 @@ return [
     'reservation_appointment' => 'Meeting Reservation',
     'reserve_appointment' => 'Reserve a Meeting',
     'appointment' => 'Meeting',
-
     'error_saving_time' => 'Error while saving time',
     'note_to_time_must_greater_from_time' => 'The end time must be greater than the start time.',
     'error_times' => 'Time error',
@@ -28,9 +26,7 @@ return [
     'success_delete_time' => 'Time successfully deleted',
     'error_delete_time' => 'Error in deleting time',
     'save_meeting_setting_success' => 'Operation successfully done.',
-
     'paid_for_x_hour' => 'Paid for :hours hours of consultation',
-
     'finish_reserve_modal_hint' => 'This action couldnt be undone.',
     'finish_reserve_modal_confirm' => 'Yes, finish it!',
     'finish_reserve_modal_cancel' => 'Cancel',
