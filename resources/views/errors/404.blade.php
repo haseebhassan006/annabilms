fa37b795dbbdb7b7d6c7d8cdf913f44dbcf6e2da
HaseebHassan <mhhassan095@gmail.com>
2021-09-15 17:37:27 -0700

    support image fixed

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,13 +5,23 @@
         $get404ErrorPageSettings = get404ErrorPageSettings();
     @endphp
 
-    <section class="my-50 container text-center">
+    {{-- <section class="my-50 container text-center">
         <div class="row justify-content-md-center">
             <div class="col col-md-6">
                 <img src="{{ $get404ErrorPageSettings['error_image'] ?? '' }}" class="img-cover " alt="">
             </div>
         </div>
 
+        <h2 class="mt-25 font-36">{{ $get404ErrorPageSettings['error_title'] ?? '' }}</h2>
+        <p class="mt-25 font-16">{{ $get404ErrorPageSettings['error_description'] ?? '' }}</p>
+    </section> --}}
+    <section class="cart-banner position-relative text-center">
+
+        <div class="row justify-content-md-center">
+            <div class="col col-md-6">
+                <img src="{{ $get404ErrorPageSettings['error_image'] ?? '' }}" class="img-cover " alt="">
+            </div>
+        </div>
         <h2 class="mt-25 font-36">{{ $get404ErrorPageSettings['error_title'] ?? '' }}</h2>
         <p class="mt-25 font-16">{{ $get404ErrorPageSettings['error_description'] ?? '' }}</p>
     </section>
